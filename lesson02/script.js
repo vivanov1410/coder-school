e77d28a38699bb711a56ae8f3b6401cd6012f274
Viacheslav Ivanov <slava.eth@gmail.com>
2015-02-03 19:30:46 -0800

    init commit

---
+++ lesson02/script.js
@@ -0,0 +1,40 @@
+var number1 = 10; // integer
+var number2 = 5.5; // float
+
+var name = 'Slava'; // string
+
+// console.log(number1);
+// console.log(number2);
+// console.log(name);
+// console.log((1+3)*10);
+
+var sum = number1 + number2;
+// console.log(sum);
+
+var firstName = 'Slava';
+var lastName = 'Ivanov';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+var wrongText1 = "This ain't good";
+var wrongText2 = 'This ain\'t good';
+// console.log(wrongText1);
+// console.log(wrongText2);
+
+var newLine = 'This is first\nThis is second'; // \n is new line
+// console.log(newLine);
+
+var bigger1 = number1 > number2; // boolean
+var bigger2 = 4 > 1;
+var equal = 10 === 10;
+var notEqual = 10 !== 10;
+// console.log(bigger);
+// console.log(equal);
+// console.log(notEqual);
+
+
+
+
+
+
+
