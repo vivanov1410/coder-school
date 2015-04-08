827a8a01e75fc4539fb18b767fee414d3e555000
Viacheslav Ivanov <slava.eth@gmail.com>
2015-04-08 03:48:57 -0700

    added info about arrays and functions to lesson03

--- lesson03/lesson03.js
+++ lesson03/lesson03.js
@@ -5,41 +5,82 @@
 // var a = 'hello';
 // var b = 4;
 
-// var arr1 = [1, 2, 3, 55, 16];
-// var fruits = ['apple', 'pineaple', 'cucumber', 'watermelon'];
+// var arr = [1, 2, 3, 55, 16];
+// var fruits = ['apple', 'pineaplex', 'cucumber', 'watermelon'];
+// // index         0          1           2            3
+
+// console.log(fruits[0]); // return first element of array
+// console.log(fruits[1]); // return second element of array
+// console.log(fruits.length); // return a length of array
+// console.log(fruits[fruits.length - 1]); // return a last element of array
+
+// fruits.push('banana'); // add new element to array
+// console.log(fruits.length);
+// console.log(fruits);
+
+// fruits[1] = 'pineaple';
+// console.log(fruits);
+
 // // min = 0, max = fruits.length
 // var randomIndex = random(0, fruits.length);
 // console.log(fruits[randomIndex]);
 
-function print(message) {
-  console.log(message);
+/**
+ * Functions
+ */
+
+function printStats(name, health, armor) {
+  console.log('The greatest ' + name);
+  console.log(health);
+  console.log(armor);
 }
 
-// print('Privet vsem vam!');
+// printStats('Slava', 100, 50);
+// printStats('Misha', 200, 100);
+// printStats('Sveta', 300, 150);
 
-function square(number) {
-  return number*number;
+// function print(message) {
+//   console.log(message);
+// }
+
+// // print('Privet vsem vam!');
+
+function double(number) {
+  return number * 2;
 }
 
-var x = square(10);
-// console.log(x);
-print(x);
-// console.log(square(10));
-// console.log(square(30));
+// console.log(double(101));
 
-function min(a, b) {
-  if(a < b) {
-    return a;
-  } else {
-    return b;
-  }
+function increasePopulation(population, birthRate) {
+  return population * birthRate;
 }
 
-console.log(min(4, 8));
-console.log(min(10, 3));
+console.log(increasePopulation(100, 1.5));
+console.log(increasePopulation(46342, 1.3372));
 
-// var x = 10;
-// console.log(x*x);
+// function square(number) {
+//   return number*number;
+// }
 
-// var y = 145;
-// console.log(y*y);
+// var x = square(10);
+// // console.log(x);
+// print(x);
+// // console.log(square(10));
+// // console.log(square(30));
+
+// function min(a, b) {
+//   if(a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+// console.log(min(4, 8));
+// console.log(min(10, 3));
+
+// // var x = 10;
+// // console.log(x*x);
+
+// // var y = 145;
+// // console.log(y*y);
