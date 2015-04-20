b517eaaf2e96fdeff9a768a1dc316cad9203f7bd
Mihail Romanyuk <mihail09995@gmail.com>
2015-04-21 00:33:11 +0300

    add homework

--- lesson04/extra-homework.js
+++ lesson04/extra-homework.js
@@ -1,10 +1,17 @@
-/**
- * Create a function FizzBaz() that iterates the integers from 1 to 100.
- * Fut for multiples of 3 print "Fizz" instead of the number
- * For the multiples of 5 print "Buzz"
- * For numbers which are multiples of both 3 and 5 print "FizzBuzz"
- */
 
-/**
- * Write a JavaScript program to sum the multiples of 3 and 5 under 1000
- */
+
+for(var i = 0; i <= 100; i++) {
+   console.log(i);
+}
+
+
+// *
+//  * Create a function FizzBaz() that iterates the integers from 1 to 100.
+//  * Fut for multiples of 3 print "Fizz" instead of the number
+//  * For the multiples of 5 print "Buzz"
+//  * For numbers which are multiples of both 3 and 5 print "FizzBuzz"
+//  */
+
+//* Write a JavaScript program to sum the multiples of 3 and 5 under 1000
+ 
+
--- lesson04/homework.js
+++ lesson04/homework.js
@@ -6,15 +6,15 @@
  * sum(1, 5) -> 15
  */
 
-function sum(start, end) {
-  var s = 0;
-  for(var i = start; i <= end; i++) {
-   s = s + i;
-  }
+// function sum(start, end) {
+//   var s = 0;
+//   for(var i = start; i <= end; i++) {
+//    s = s + i;
+//   }
 
-  return s;
-}
-console.log('Total: ' + sum(3, 5));
+//   return s;
+// }
+// console.log('Total: ' + sum(3, 5));
 
 /**
  * Create a function factorial(number)
@@ -25,14 +25,14 @@ console.log('Total: ' + sum(3, 5));
  */
 //factorial произведение всех натуральных чисел от 1 до number
 
-function factorial(number) {
-  var product = 1;
-  for(var i = 1; i <= number; i++) {
-    product = product * i;
-  }
-  return product;
-}
-console.log(factorial(10));
+// function factorial(number) {
+//   var product = 1;
+//   for(var i = 1; i <= number; i++) {
+//     product = product * i;
+//   }
+//   return product;
+// }
+// console.log(factorial(10));
 
 /**
  * Create a function ladder(steps)
@@ -57,14 +57,14 @@ console.log(factorial(10));
 // }
 // ladder(4);
 
-function ladder(steps, symbol) {
-  var zv = symbol;
-  for(var i = 1; i <= steps; i++) {
-    console.log(zv);
-    zv = zv + symbol;
-  }
-}
-ladder(10, '?');
+// function ladder(steps, symbol) {
+//   var zv = symbol;
+//   for(var i = 1; i <= steps; i++) {
+//     console.log(zv);
+//     zv = zv + symbol;
+//   }
+// }
+// ladder(10, '?');
 
 
 /**
@@ -103,23 +103,64 @@ ladder(10, '?');
 
 //////////////////////////////////////////////вариант 2
 
-function tree(rows) {
-  var context = '*';
-  var maxLength = (rows*2 - 1);
-  var totalSpaces = (maxLength - 1) / 2;
+// function tree(rows) {
+//   var context = '*';
+//   var maxLength = (rows*2 - 1);
+//   var totalSpaces = (maxLength - 1) / 2;
 
-  for(var i = 0; i < rows; i++) {
-    var spaces = '';
-    for(var j = 0; j < totalSpaces; j++) {
-      spaces += ' ';
+//   for(var i = 0; i < rows; i++) {
+//     var spaces = '';
+//     for(var j = 0; j < totalSpaces; j++) {
+//       spaces += ' ';
+//     }
+
+//     console.log(spaces + context);
+
+//     context += '**';
+//     totalSpaces = totalSpaces - 1;
+//   }
+// }
+
+// tree(5);
+
+
+// function FizzBuzz(number) {
+//   for(var i = 1; i <= number; i++) {
+//     if(i % 3 === 0 && i % 5 === 0) {
+//       console.log(i + ' FizzBuzz');
+//     } else if(i % 5 === 0) {
+//       console.log(i + ' Buzz');
+//     } else if(i % 3 === 0) {
+//       console.log(i + ' Fizz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+// FizzBuzz(100);
+
+
+
+function sum(number) {
+  var total = 0;
+  var sumOf3 = 0;
+  var sumOf5 = 0;
+
+  for(var i = 1; i <= number; i++) {
+    if(i % 5 === 0) {
+      sumOf5 = sumOf5 + i;
+    } else if(i % 3 === 0) {
+      sumOf3 = sumOf3 + i;
     }
-
-    console.log(spaces + context);
-
-    context += '**';
-    totalSpaces = totalSpaces - 1;
   }
+  total = (sumOf3 + sumOf5);
+
+  return total;
 }
 
-tree(5);
+console.log('totale ' + sum(100));
 
+// выучить арифмитическую прогрессию
+// ctrl + enter - вставить пустую строку с любого места кода
+
+console.log(333 * 3);
