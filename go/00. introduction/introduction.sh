88d564c2b40b551276cbbc540273ca467956779a
Viacheslav Ivanov <slava.eth@gmail.com>
2015-06-24 02:09:04 -0700

    added lesson 02

--- go/00. introduction/introduction.sh
+++ go/00. introduction/introduction.sh
@@ -13,5 +13,11 @@ export PATH=$PATH:$GOPATH/bin
 # create user base path
 mkdir -p $GOPATH/src/github.com/user
 
-## Running
+## Compiling
+
+# run main package files
 go run file.go
+
+## build non-main page files
+go build file.go
+./file
--- go/01. variables/variables.go
+++ go/01. variables/variables.go
@@ -3,18 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	name = "Slava"
+	// https://golang.org/ref/spec#Types
 
-	var name2 int = 3
+	// String Type
+	var name string   // declare a variable
+	name = "Slava"    // set value to variable
+	fmt.Println(name) // print out value of the variable
 
-	var number int
-	number = 3
+	var name2 string = "Misha"       // declare and initialize variable
+	fmt.Println("My name is", name2) // print out value of the variable
 
-	fmt.Println(name, name2, number)
+	name3 := "Sveta"                    // another way to declare and initialize a variable
+	fmt.Println("We all love " + name3) // print out value of the variable
 
-	fmt.Println("Введите имя:")
-	var newName string
-	fmt.Scan(&newName)
-	fmt.Println("Ваше имя", newName)
+	// Integer Type
+	var numberOfChairs int32 // -2147483648 to 2147483647
+	numberOfChairs = 3
+	fmt.Println("I have", numberOfChairs, "chairs")
+
+	var realNumberOfChairs uint8 // 0 - 255
+	realNumberOfChairs = 15
+	fmt.Println("I really only have", realNumberOfChairs, "chairs")
+
+	// Float Type
+	var radius float32 = 300.5
+	fmt.Println("My circle has a radius of", radius, "meters")
+
+	// Boolean Type
+	var isOn bool = true
+	fmt.Println("Is my light is on?", isOn)
+
+	isDead := false
+	fmt.Println("Is enemy dead?", isDead)
 }
