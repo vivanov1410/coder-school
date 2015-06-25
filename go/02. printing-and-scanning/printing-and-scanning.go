e4ac0face54b7a4cb906de05a216b368de74731f
Viacheslav Ivanov <slava.eth@gmail.com>
2015-06-24 20:53:29 -0700

    updated 02 lesson

---
+++ go/02. printing-and-scanning/printing-and-scanning.go
@@ -0,0 +1,31 @@
+// PRINTING
+// Print function docs
+
+// godoc Print
+
+// print out any data using Println
+
+// print out general data using %v
+
+// print out boolean data using %t
+
+// print out integer data using %d, binary format %b
+
+// print out float data using %f
+
+// print out float data with precision using %f
+
+// print out string data using %s
+
+// special symbols: \n, \t
+
+// SCANNING
+// Scan function docs
+
+// godoc Scan
+
+// scan using Scan
+
+// scan formatted data using Scanf
+
+// scan and print examples
